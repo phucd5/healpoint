c664ba09ebda8d4267f63561503250073b2dac34
yilinli <yilinli@mit.edu>
2023-09-16 16:42:59 -0400

    changed route

--- app/src/App.js
+++ app/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<TestComponent />} />
+          <Route path="/" element={<Home />} />
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
         </Routes>
