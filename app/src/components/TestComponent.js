8828a4812b8af38809f75e0a191aa3a8fd7fa262
yilinli <yilinli@mit.edu>
2023-09-16 12:45:08 -0400

    created bootstrap button

--- app/src/components/TestComponent.js
+++ app/src/components/TestComponent.js
@@ -1,5 +1,8 @@
+import Button from "react-bootstrap/Button"
+import "bootstrap/dist/css/bootstrap.min.css"
+
 const TestComponent = (props) => {
-	return <div>Hello World</div>;
+	return <div>Hello World <Button>Button here</Button></div>;
 };
 
 export default TestComponent;
