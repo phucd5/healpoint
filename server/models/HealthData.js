547b5717450701baf89c49694a30e0392a8b5606
“Calvin <Calvinli0824@gmail.com>
2023-09-16 15:19:01 -0400

    added health model

--- server/models/HealthData.js
+++ server/models/HealthData.js
@@ -1,1 +1,18 @@
-import
+import mongoose from 'mongoose';
+
+const healthDataSchema = new mongoose.Schema(
+    {
+        lifestyle: {
+            type: [],
+            required: true
+        }, 
+        Conditions: {
+            type: [],
+            required: true
+        }
+
+    }
+)
+
+const HealthData = mongoose.model("HealthData", healthDataSchema);
+export default HealthData;
