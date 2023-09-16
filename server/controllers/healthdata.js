02e113bef3e82c0923e9b8c5e89f2fb858c18be0
“Calvin <Calvinli0824@gmail.com>
2023-09-16 16:27:53 -0400

    fixed healthdata controllers

--- server/controllers/healthdata.js
+++ server/controllers/healthdata.js
@@ -1,67 +1,100 @@
-import asyncHandler from 'express-async-handler'
 import HealthData from '../models/HealthData.js'
+import { handleNotFound, handleBadRequest, handleSuccess, handleServerError } from '../utils/handlers.js';
 
 // @desc    get HealthData
 // @route   Get /HealthData
 // @access  Private
-export const getHealthData = asyncHandler(async (req, res) => {
-    const users = await HealthData.find();
-    res.json(HealthData);
-})
+export const getHealthData = async (req, res) => {
+    try {
+
+        const HealthData = await HealthData.find();
+    handleSuccess(res, HealthData);
+
+    } catch (error) {
+        handleServerError(res, error);
+    }
+    
+}
 
 // @desc    update HealthData
 // @route   Put /HealthData
 // @access  Private
-export const putHealthData = asyncHandler(async (req, res) => {
-    const { lifestyle, conditions } = req.body;
+export const putHealthData = async (req, res) => {
+    try {
+
+        const { lifestyle, conditions } = req.body;
     const update = await HealthData.findById(req.params.id);
 
     if (!update) {
-        res.status(400);
-        throw new Error('user not found');
+        handleNotFound(res, 'HealthData not found');
     }
 
     const updatedHealthData = await HealthData.findByIdAndUpdate(req.params.id, { lifestyle, conditions }, {new: true});
-    res.json(updatedHealthData);
-})
+    handleSuccess(res, updatedHealthData);
+        
+    } catch (error) {
+        handleServerError(res, error);
+    }
+    
+}
 
 // @desc    delete HealthData
 // @route   Delete /HealthData
 // @access  Private
-export const deleteHealthData = asyncHandler(async (req, res) => {
-    const getrid = await HealthData.findById(req.params.id);
+export const deleteHealthData = async (req, res) => {
+    try {
+
+        const getrid = await HealthData.findById(req.params.id);
 
     if (!getrid) {
-        res.status(400);
-        throw new Error('user not found');
+        handleNotFound(res, 'HealthData not found');
     }
 
-    const deletedHealthData = await HealthData.findByIdAndDelete(req.params.id)
-    res.json(deleteduser)
-})
-
-export const getLifestyle = asyncHandler(async (req, res) => {
-
-      const healthData = await HealthData.findOne({}, 'lifestyle');
-      
-      if (!healthData) {
-        return res.status(404).json({ message: 'No health data found' });
-      }
-  
-
-      res.status(200).json({ lifestyle: healthData.lifestyle });
+    const deletedHealthData = await HealthData.findByIdAndDelete(req.params.id);
+    handleSuccess(res, deletedHealthData);
+        
+    } catch (error) {
+        handleServerError(res, error);
     }
-)
-
-export const getCondition = asyncHandler(async (req, res) => {
-
-    const healthData = await HealthData.findOne({}, 'condition');
     
-    if (!healthData) {
-      return res.status(404).json({ message: 'No health data found' });
+}
+
+export const getLifestyle = async (req, res) => {
+
+    try {
+        
+        const healthData = await HealthData.findOne({}, 'lifestyle');
+      
+        if (!healthData) {
+          handleNotFound(res, 'HealthData not found');
+        }
+    
+        handleSuccess(res, { lifestyle: healthData.lifestyle });
+
+    } catch (error) {
+
+        handleServerError(res, error);
+        
+    }
+}
+
+export const getCondition = async (req, res) => {
+
+    try {
+
+        const healthData = await HealthData.findOne({}, 'condition');
+    
+        if (!healthData) {
+          handleNotFound(res, 'HealthData not found');
+        }
+    
+    
+        handleSuccess( res, { condition: healthData.condition });
+        
+    } catch (error) {
+        
+        handleServerError(res, error);
+
     }
 
-
-    res.status(200).json({ condition: healthData.condition });
-  }
-)
+}
